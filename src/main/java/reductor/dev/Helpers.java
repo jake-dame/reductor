18eb30fcf55cbd4ebe86201e2f97b92ce9b72a08
jake-dame <u0858882@utah.edu>
2025-10-04 23:30:42 -0600

    openWith can now be used with windows and linux; split openWith into separate functions, do away with static constant args; clean up play using try with resources, add sleep instead of constant checks

--- src/main/java/reductor/dev/Helpers.java
+++ src/main/java/reductor/dev/Helpers.java
@@ -2,91 +2,93 @@ package reductor.dev;
 
 
 import javax.sound.midi.*;
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 
 public class Helpers {
 
 
-    /** For use in conjunction with {@link Helpers#openWith}. */
-    public static final String MUSESCORE = "MuseScore 4.app";
-
-    /** For use in conjunction with {@link Helpers#openWith}. */
-    public static final String GARAGEBAND = "GarageBand.app";
+    private static final String MUSESCORE = "MuseScore 4.app";
+    private static final String GARAGEBAND = "GarageBand.app";
 
 
-    /**
-     * Opens a valid MIDI file with a particular app.
-     *
-     * @param file The MIDI file to open
-     * @param app The name of the application like "appName.app"
-     */
-    static void openWith(File file, String app) throws IOException {
+    private Helpers() {}
 
-        if (!app.equals(GARAGEBAND)  &&  !app.equals(MUSESCORE)) {
-            throw new RuntimeException("use one of: GARAGEBAND, MUSESCORE");
-        }
 
-        // openWith() is only written for macOS right now
+    public static void openWithGarageBand(Path path) {
+        openWith(path, GARAGEBAND);
+    }
+
+    public static void openWithMuseScore(Path path) {
+        openWith(path, MUSESCORE);
+    }
+
+    private static void openWith(Path path, String app) {
+
+        ProcessBuilder pb;
+
         String osName = System.getProperty("os.name").toLowerCase();
-        if (!osName.contains("mac")) {
-            System.err.println("openWith() is only supported on macOS. Skipping.");
+        if (osName.contains("mac")) {
+            pb = new ProcessBuilder("open", "-a", "/Applications/" + app, path.toString());
+        } else if (osName.contains("win")) {
+            pb = new ProcessBuilder("cmd", "/c", "start", "\"\"", path.toString());
+        } else if (osName.contains("nux")) {
+            pb = new ProcessBuilder("xdg-open", path.toString());
+        } else {
+            System.err.println("no openwith for you >:[");
             return;
         }
 
+        try {
+            // inheritIO makes it so console messages aren't shot into the void (i.e. subprocess)
+            pb.inheritIO().start();
+        } catch (IOException e) {
+            System.err.println("couldn't open MuseScore 4 or GarageBand. Check app location/installation");
+        }
 
-        checkMidiFileExtension(file);
-
-        ProcessBuilder processBuilder = new ProcessBuilder(
-                "open",
-                "-a",
-                "/Applications/" + app,
-                file.getAbsolutePath()
-        );
-
-        processBuilder.start();
     }
 
-    /**
-     * Helper for {@link #openWith}. Checks if a file has the correct
-     * ".mid" extension.
-     *
-     * @param file The file to check.
-     */
-    private static void checkMidiFileExtension(File file) {
-        String outFileName = file.getName();
-        int periodIndex = outFileName.lastIndexOf('.');
-        if (periodIndex == -1
-                || !outFileName.substring(periodIndex + 1).equals("mid")) {
-            throw new RuntimeException("out file should have '.mid' extension");
-        }
+    public static void play(Path path) throws InvalidMidiDataException, IOException {
+        Sequence seq = MidiSystem.getSequence(path.toFile());
+        play(seq);
     }
 
     /**
      * Invokes the {@link javax.sound.midi.Sequencer}'s playback functionality.
+     * <p>
+     * If you are on macOS, this will be Gervill.
      *
      * @param sequence The {@link javax.sound.midi.Sequence} to play
      */
+    @SuppressWarnings("BusyWait")
     public static void play(Sequence sequence) throws InvalidMidiDataException {
 
-        Sequencer sequencer;
-        try {
-            sequencer = MidiSystem.getSequencer();
-            sequencer.setSequence(sequence);
+        try (Sequencer sequencer = MidiSystem.getSequencer()) {
+
             sequencer.open();
+            sequencer.setSequence(sequence);
             sequencer.start();
+
+            while (sequencer.isRunning()) {
+                try {
+                    // nothing in Sequencer blocks, ignore warning
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {}
+            }
+
         } catch (MidiUnavailableException e) {
             throw new RuntimeException(e);
         }
 
-        while (true) {
-            if (!sequencer.isRunning()) {
-                sequencer.close();
-                return;
-            }
-        }
+    }
 
+    public static void checkMidiFileExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int dot = fileName.lastIndexOf('.');
+        if (dot == -1 || !fileName.substring(dot + 1).equals("mid")) {
+            throw new RuntimeException("out file should have '.mid' extension");
+        }
     }
 
 

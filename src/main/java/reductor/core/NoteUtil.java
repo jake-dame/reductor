224297870f9cc1e8900f4b1da86b171eefba59e7
jake-dame <u0858882@utah.edu>
2025-10-06 22:08:54 -0600

    toList(): this is purely utility, static; didn't belong in Note

---
+++ src/main/java/reductor/core/NoteUtil.java
@@ -0,0 +1,21 @@
+package reductor.core;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NoteUtil {
+
+    /**
+     * Given something like List.of("C", "E", "G", "Bb"), pops out a list of Note objects.
+     * Helpful in test creation.
+     */
+    public static ArrayList<Note> toList(List<String> strings) {
+        ArrayList<Note> out = new ArrayList<>();
+        for (String str : strings) { out.add( Note.builder().pitch(str).build() ); }
+        return out;
+    }
+
+
+}

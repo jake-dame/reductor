5abf19a521ce68484e597ea743526f150a2dc940
jake-dame <u0858882@utah.edu>
2025-10-06 22:07:17 -0600

    KeySignature: redesign, split into Util class, regex for String parser; moving towards record setup

---
+++ src/main/java/reductor/core/KeySignatureUtil.java
@@ -0,0 +1,89 @@
+package reductor.core;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class KeySignatureUtil {
+
+    private final static Pattern PATTERN_KEY_SIG = Pattern.compile("""
+        (?i)^
+        \\s*
+        (?<letter>[A-G])
+        \\s*
+        (?<accidental>#|b|sharp|flat)?
+        \\s*
+        (?<mode>m|maj(or)?|min(or)?)?
+        \\s*
+        $
+        """);
+
+    static final Map<Integer, String> keysMajorItoS;
+    static final Map<Integer, String> keysMinorItoS;
+    static final Map<String, Integer> keysMajorStoI;
+    static final Map<String, Integer> keysMinorStoI;
+
+    static {
+
+        keysMajorItoS = Map.ofEntries(
+                Map.entry(-7, "Cb"),
+                Map.entry(-6, "Gb"),
+                Map.entry(-5, "Db"),
+                Map.entry(-4, "Ab"),
+                Map.entry(-3, "Eb"),
+                Map.entry(-2, "Bb"),
+                Map.entry(-1, "F"),
+                Map.entry(0, "C"),
+                Map.entry(1, "G"),
+                Map.entry(2, "D"),
+                Map.entry(3, "A"),
+                Map.entry(4, "E"),
+                Map.entry(5, "B"),
+                Map.entry(6, "F#"),
+                Map.entry(7, "C#")
+        );
+
+        keysMinorItoS = Map.ofEntries(
+                Map.entry(-7, "ab"),
+                Map.entry(-6, "eb"),
+                Map.entry(-5, "bb"),
+                Map.entry(-4, "f"),
+                Map.entry(-3, "c"),
+                Map.entry(-2, "g"),
+                Map.entry(-1, "d"),
+                Map.entry(0, "a"),
+                Map.entry(1, "e"),
+                Map.entry(2, "b"),
+                Map.entry(3, "f#"),
+                Map.entry(4, "c#"),
+                Map.entry(5, "g#"),
+                Map.entry(6, "d#"),
+                Map.entry(7, "a#")
+        );
+
+        keysMajorStoI = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : keysMajorItoS.entrySet()) {
+            keysMajorStoI.put(entry.getValue(), entry.getKey());
+        }
+
+        keysMinorStoI = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : keysMinorItoS.entrySet()) {
+            keysMinorStoI.put(entry.getValue(), entry.getKey());
+        }
+
+    }
+
+    // ========================================  METHODS ======================================== //
+
+    public static Matcher parse(String str) {
+        Matcher matcher = PATTERN_KEY_SIG.matcher(str);
+        if (!matcher.matches()) {
+            throw new IllegalStateException("invalid key sig string: " + str);
+        }
+        return matcher;
+    }
+
+}

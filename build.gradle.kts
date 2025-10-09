6f44dc6dccf397c244e94a766d004ae199ab0857
jake-dame <u0858882@utah.edu>
2025-10-09 11:06:40 -0600

    adding jqwik, assertJ dependencies; logging tweaks; comments on dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,56 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import java.nio.file.Paths
-
-plugins {
-    java
-}
-
-apply(from = rootProject.file("golden.gradle.kts"))
 
 group = "reductor"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    java
+    application
+}
 
 repositories {
     mavenCentral()
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(25))
-    }
-}
-
 dependencies {
+    // ProxyMusic
     implementation("org.audiveris:proxymusic:4.0.3")
+
+    // SLF4J
     implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
+
+    // LogBack
     implementation("ch.qos.logback:logback-classic:1.5.13")
 
     // Gradle 9.1.0 changed some stuff with how you need to explicitly pull in API + engine,
     // seems they are trying to migrate people to `jvm-test-suite` plug-in, but that isn't stable yet
-    // Keeping this here (just from their docu)
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+
+
+    // PBT library, `Arbitraries` and `Combinator` classes
+    testImplementation("net.jqwik:jqwik:1.9.3")
+    testRuntimeOnly("net.jqwik:jqwik-engine:1.9.3")
+
+    // Fluent assertions library
+    testImplementation("org.assertj:assertj-core:3.27.6")
+}
+
+// ======================================== CUSTOM TASKS ======================================== //
+
+
+apply(from = rootProject.file("golden.gradle.kts"))
+
+
+application { mainClass = "reductor.Main" }
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(25)) }
+}
+
+tasks.wrapper {
+    gradleVersion = "9.1.0"
 }
 
 tasks.test {

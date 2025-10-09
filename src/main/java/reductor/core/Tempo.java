20854c49bd238150b5d64d0b1de46259869c6333
jake-dame <u0858882@utah.edu>
2025-10-09 11:24:34 -0600

    convert TimeSignature and Tempo to record classes; their pure utility functions have been moed to core/Util.java

--- src/main/java/reductor/core/Tempo.java
+++ src/main/java/reductor/core/Tempo.java
@@ -1,36 +1,18 @@
 package reductor.core;
 
-public class Tempo implements Ranged {
-
-
-    private final Range range;
-
-    private int bpm;
-
-
-    public Tempo(int bpm, Range range) {
-        this.bpm = bpm;
-        this.range = new Range(range);
-    }
+public record Tempo(int bpm, Range range) implements Ranged {
 
     public Tempo(Tempo other) {
-        this.bpm = other.bpm;
-        this.range = new Range(other.range);
+        this(other.bpm, new Range(other.range));
     }
 
     public int getBpm() { return this.bpm; }
-    public void setBpm(int val) {
-        if (val < 0) { val = 0; }
-        this.bpm = val;
-    }
 
     @Override
     public Range getRange() { return new Range(this.range); }
 
     @Override
-    public String toString() {
-        return this.range + " " + this.bpm + " bpm";
-    }
+    public String toString() { return "%s: %s bpm".formatted(range, bpm); }
 
 
 }
